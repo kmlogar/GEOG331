2cc9d2b7ad0bc006421ebc439ab158b2bacf5a4a
Kayla Logar '20 <klogar@colgate.edu>
2020-01-24 12:11:23 -0500

    added an R script for activity 1

--- Activity1/activity1_script.r
+++ Activity1/activity1_script.r
@@ -1,3 +1,26 @@
 # Activity 1
 print("hello R world")
 
+# 7 raised to the fifth power
+7^5
+
+# 10 plus 365
+10 + 365
+
+# 2 minus 8
+2 - 8
+
+# assign a number to the variable a
+a <- 2446
+
+# multiply number by 13
+13 * a
+
+# divide number by 2
+a / 2
+
+# make a vector of numbers 
+b <- c(2446, 40, 10000)
+
+#divide all numbers by 2
+b/2

f749094cb6da5c36db3960fc9a2cea8fafe3d951
Kayla Logar '20 <klogar@colgate.edu>
2020-01-29 12:14:20 -0500

    Activity2

---
+++ Activity2/activity2_script.R
@@ -0,0 +1,189 @@
+#make a vector of tree heights in meters
+heights <- c(30, 41, 20, 22)
+#convert to cm
+heights_cm <- heights * 100
+heights_cm
+
+#Look at the first tree height
+heights[1]
+#Look at the 2nd and 3rd tree heights
+heights[2:3]
+
+#get more info on the matrix function
+help("matrix")
+
+#set up a matrix with 2 columns and fill in by rows
+#first argument is the vector of numbers to fill in the matrix
+Mat <- matrix(c(1, 2, 3, 4, 5, 6), ncol = 2, byrow = TRUE)
+Mat
+
+#set up a matrix that fills in by columns
+#first argument is the vector of numbers to fill in the matrix
+Mat.bycol <- matrix(c(1, 2, 3, 4, 5, 6), ncol = 2, byrow = FALSE)
+Mat.bycol
+
+#subset the matrix to look at row 1, column 2
+Mat.bycol[1,2]
+
+#Look at all values in row 1
+Mat.bycol[1,]
+
+#Look at all values in column 2
+Mat.bycol[,2]
+
+#read in weather station file from the data folder
+datW <- read.csv("y:\\Students\\klogar\\a02\\2011124.csv")
+
+#get more information about the dataframe
+str(datW)
+
+#specify a column with a proper date format
+#note the format here dataframe$column
+datW$dateF <- as.Date(datW$DATE, "%Y-%m-%d")
+#google date formatting in r to find more options and learn more
+
+#create a date column by reformatting the date to only include years
+#and indicating that it should be treated as numeric data
+datW$year <- as.numeric(format(datW$dateF, "%Y"))
+
+#find out all unique site names
+levels(datW$NAME)
+
+#look at the mean maximum temperature for Aberdeen
+mean(datW$TMAX[datW$NAME == "ABERDEEN, WA US"])
+
+#look at the mean maximum temperature for Aberdeen 
+#with na.rm argument set to true to ignore NA
+mean(datW$TMAX[datW$NAME == "ABERDEEN, WA US"], na.rm = TRUE)
+
+#calculate the average daily temperature
+#This temperature will be halfway between the minimum and maximum temperature
+datW$TAVE <- datW$TMIN + ((datW$TMAX-datW$TMIN)/2)
+
+#get the mean across all sites
+#the by function is a list of one or more variables to index over.
+#FUN indicates the function we want to use
+#if you want to specify any function specific arguments use a comma and add them after the function
+#here we want to use the na.rm arguments specific to 
+averageTemp <- aggregate(datW$TAVE, by=list(datW$NAME), FUN="mean", na.rm=TRUE)
+averageTemp
+
+#change the automatic output of column names to be more meaningful
+#note that MAAT is a common abbreviation for Mean Annual Air Temperature
+colnames(averageTemp) <- c("NAME", "MAAT")
+averageTemp
+
+#convert level to number for factor data type
+#you will have to reference the level output or look at the row of data to see the character designation.
+datW$siteN <- as.numeric(datW$NAME)
+
+#make a histogram for the first site in our levels
+#main = is the title name argument
+#Here you want to paste the actual name of the factor not the numeric index
+#since that will be more meaningful.
+hist(datW$TAVE[datW$siteN == 1],
+     freq = FALSE, 
+     main = paste(levels(datW$NAME)[1]),
+     xlab = "Average daily temperature (degrees C)",
+     ylab = "Relative frequency", 
+     col = "grey50",
+     border = "white")
+
+##################################################
+##             ABERDEEN HISTOGRAM               ##
+##################################################
+
+#make a histogram for the first site in our levels, Aberdeen
+#main = is the title name argument.
+#Here you want to paste the actual name of the factor not the numeric index
+#since that will be more meaningful.
+hist(datW$TAVE[datW$siteN == 1],
+     freq = FALSE, 
+     main = paste(levels(datW$NAME)[1]),
+     xlab = "Average daily temperature (degrees C)",
+     ylab = "Relative frequency", 
+     col = "grey50",
+     border = "white")
+#add mean line with red (tomato3) color
+#and thickness of 3
+abline(v = mean(datW$TAVE[datW$siteN == 1], na.rm = TRUE),
+       col = "tomato3",
+       lwd = 3)
+#add standard deviation line below the mean with red (tomato3) color
+#and thickness of 3
+abline(v = mean(datW$TAVE[datW$siteN == 1], na.rm=TRUE) - sd(datW$TAVE[datW$siteN == 1], na.rm=TRUE),
+       col = "tomato3",
+       lty = 3,
+       lwd = 3)
+#add standard deviation line above the mean with red (tomato3) color
+#and thickness of 3
+abline(v = mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE) + sd(datW$TAVE[datW$siteN == 1], na.rm=TRUE),
+       col = "tomato3",
+       lty = 3,
+       lwd = 3)
+
+##################################################
+##             LIVERMORE HISTOGRAM              ##
+##################################################
+
+#make a histogram for the second site in our levels, Livermore
+#main = is the title name argument.
+#Here you want to paste the actual name of the factor not the numeric index
+#since that will be more meaningful.
+hist(datW$TAVE[datW$siteN == 2],
+     freq = FALSE, 
+     main = paste(levels(datW$NAME)[2]),
+     xlab = "Average daily temperature (degrees C)",
+     ylab = "Relative frequency", 
+     col = "grey50",
+     border = "white")
+#add mean line with red (tomato3) color
+#and thickness of 3
+abline(v = mean(datW$TAVE[datW$siteN == 2], na.rm = TRUE),
+       col = "tomato3",
+       lwd = 3)
+#add standard deviation line below the mean with red (tomato3) color
+#and thickness of 3
+abline(v = mean(datW$TAVE[datW$siteN == 2], na.rm=TRUE) - sd(datW$TAVE[datW$siteN == 2], na.rm=TRUE),
+       col = "tomato3",
+       lty = 3,
+       lwd = 3)
+#add standard deviation line above the mean with red (tomato3) color
+#and thickness of 3
+abline(v = mean(datW$TAVE[datW$siteN == 2],na.rm=TRUE) + sd(datW$TAVE[datW$siteN == 2], na.rm=TRUE),
+       col = "tomato3",
+       lty = 3,
+       lwd = 3)
+
+##################################################
+##   MANDAN EXPERIMENT STATION HISTOGRAM        ##
+##################################################
+
+#make a histogram for the third site in our levels, Mandan Experiment Station
+#main = is the title name argument.
+#Here you want to paste the actual name of the factor not the numeric index
+#since that will be more meaningful.
+hist(datW$TAVE[datW$siteN == 2],
+     freq = FALSE, 
+     main = paste(levels(datW$NAME)[2]),
+     xlab = "Average daily temperature (degrees C)",
+     ylab = "Relative frequency", 
+     col = "grey50",
+     border = "white")
+#add mean line with red (tomato3) color
+#and thickness of 3
+abline(v = mean(datW$TAVE[datW$siteN == 2], na.rm = TRUE),
+       col = "tomato3",
+       lwd = 3)
+#add standard deviation line below the mean with red (tomato3) color
+#and thickness of 3
+abline(v = mean(datW$TAVE[datW$siteN == 2], na.rm=TRUE) - sd(datW$TAVE[datW$siteN == 2], na.rm=TRUE),
+       col = "tomato3",
+       lty = 3,
+       lwd = 3)
+#add standard deviation line above the mean with red (tomato3) color
+#and thickness of 3
+abline(v = mean(datW$TAVE[datW$siteN == 2],na.rm=TRUE) + sd(datW$TAVE[datW$siteN == 2], na.rm=TRUE),
+       col = "tomato3",
+       lty = 3,
+       lwd = 3)

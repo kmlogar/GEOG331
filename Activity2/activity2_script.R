9f99dd03f374d53a0aebed94d287267f5f17ca4d
Kayla Logar <kaylal26@kaylas-mbp-2.colgate.edu>
2020-02-04 20:56:03 -0500

    update project code

--- Activity2/activity2_script.R
+++ Activity2/activity2_script.R
@@ -1,3 +1,21 @@
+#############################################
+##              QUESTION 2                 ##
+##           VECTOR EXAMPLES               ##
+#############################################
+
+#character vector
+ex.char <- c("math", "science", "reading", "writing", "social studies")
+
+#numeric vector
+ex.num <- c(1, 1, 2, 3, 5)
+
+#integer vector
+ex.int <- as.integer(ex.num)
+
+#factor vector
+ex.factor <- as.factor(ex.char)
+
+
 #make a vector of tree heights in meters
 heights <- c(30, 41, 20, 22)
 #convert to cm
@@ -32,7 +50,8 @@ Mat.bycol[1,]
 Mat.bycol[,2]
 
 #read in weather station file from the data folder
-datW <- read.csv("y:\\Students\\klogar\\a02\\2011124.csv")
+filepaths <- c("y:\\Students\\klogar\\a02\\2011124.csv", "/Users/kaylal26/Documents/ColgateAcademics/Spring2020/EnvironmentalDataScience/GitHub/GEOG331/Activity2/2011124.csv")
+datW <- read.csv(filepaths[1])
 
 #get more information about the dataframe
 str(datW)
@@ -89,6 +108,12 @@ hist(datW$TAVE[datW$siteN == 1],
      col = "grey50",
      border = "white")
 
+
+##################################################
+##               HISTOGRAM GRID                 ##
+##################################################
+par(mfrow=c(2,2))
+
 ##################################################
 ##             ABERDEEN HISTOGRAM               ##
 ##################################################
@@ -102,23 +127,23 @@ hist(datW$TAVE[datW$siteN == 1],
      main = paste(levels(datW$NAME)[1]),
      xlab = "Average daily temperature (degrees C)",
      ylab = "Relative frequency", 
-     col = "grey50",
+     col = "steelblue",
      border = "white")
-#add mean line with red (tomato3) color
+#add mean line 
 #and thickness of 3
 abline(v = mean(datW$TAVE[datW$siteN == 1], na.rm = TRUE),
-       col = "tomato3",
+       col = "gray26",
        lwd = 3)
 #add standard deviation line below the mean with red (tomato3) color
 #and thickness of 3
 abline(v = mean(datW$TAVE[datW$siteN == 1], na.rm=TRUE) - sd(datW$TAVE[datW$siteN == 1], na.rm=TRUE),
-       col = "tomato3",
+       col = "gray26",
        lty = 3,
        lwd = 3)
 #add standard deviation line above the mean with red (tomato3) color
 #and thickness of 3
 abline(v = mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE) + sd(datW$TAVE[datW$siteN == 1], na.rm=TRUE),
-       col = "tomato3",
+       col = "gray26",
        lty = 3,
        lwd = 3)
 
@@ -135,23 +160,23 @@ hist(datW$TAVE[datW$siteN == 2],
      main = paste(levels(datW$NAME)[2]),
      xlab = "Average daily temperature (degrees C)",
      ylab = "Relative frequency", 
-     col = "grey50",
+     col = "lightblue3",
      border = "white")
 #add mean line with red (tomato3) color
 #and thickness of 3
 abline(v = mean(datW$TAVE[datW$siteN == 2], na.rm = TRUE),
-       col = "tomato3",
+       col = "gray26",
        lwd = 3)
 #add standard deviation line below the mean with red (tomato3) color
 #and thickness of 3
 abline(v = mean(datW$TAVE[datW$siteN == 2], na.rm=TRUE) - sd(datW$TAVE[datW$siteN == 2], na.rm=TRUE),
-       col = "tomato3",
+       col = "gray26",
        lty = 3,
        lwd = 3)
 #add standard deviation line above the mean with red (tomato3) color
 #and thickness of 3
 abline(v = mean(datW$TAVE[datW$siteN == 2],na.rm=TRUE) + sd(datW$TAVE[datW$siteN == 2], na.rm=TRUE),
-       col = "tomato3",
+       col = "gray26",
        lty = 3,
        lwd = 3)
 
@@ -163,27 +188,232 @@ abline(v = mean(datW$TAVE[datW$siteN == 2],na.rm=TRUE) + sd(datW$TAVE[datW$siteN
 #main = is the title name argument.
 #Here you want to paste the actual name of the factor not the numeric index
 #since that will be more meaningful.
-hist(datW$TAVE[datW$siteN == 2],
+hist(datW$TAVE[datW$siteN == 3],
      freq = FALSE, 
-     main = paste(levels(datW$NAME)[2]),
+     main = paste(levels(datW$NAME)[3]),
      xlab = "Average daily temperature (degrees C)",
      ylab = "Relative frequency", 
-     col = "grey50",
+     col = "thistle",
      border = "white")
 #add mean line with red (tomato3) color
 #and thickness of 3
-abline(v = mean(datW$TAVE[datW$siteN == 2], na.rm = TRUE),
-       col = "tomato3",
+abline(v = mean(datW$TAVE[datW$siteN == 3], na.rm = TRUE),
+       col = "gray26",
        lwd = 3)
 #add standard deviation line below the mean with red (tomato3) color
 #and thickness of 3
-abline(v = mean(datW$TAVE[datW$siteN == 2], na.rm=TRUE) - sd(datW$TAVE[datW$siteN == 2], na.rm=TRUE),
-       col = "tomato3",
+abline(v = mean(datW$TAVE[datW$siteN == 3], na.rm=TRUE) - sd(datW$TAVE[datW$siteN == 2], na.rm=TRUE),
+       col = "gray26",
        lty = 3,
        lwd = 3)
 #add standard deviation line above the mean with red (tomato3) color
 #and thickness of 3
-abline(v = mean(datW$TAVE[datW$siteN == 2],na.rm=TRUE) + sd(datW$TAVE[datW$siteN == 2], na.rm=TRUE),
-       col = "tomato3",
+abline(v = mean(datW$TAVE[datW$siteN == 3],na.rm=TRUE) + sd(datW$TAVE[datW$siteN == 2], na.rm=TRUE),
+       col = "gray26",
        lty = 3,
        lwd = 3)
+
+##################################################
+##        MORMON FLAT STATION HISTOGRAM         ##
+##################################################
+
+#make a histogram for the third site in our levels, Mormon Flat
+#main = is the title name argument.
+#Here you want to paste the actual name of the factor not the numeric index
+#since that will be more meaningful.
+hist(datW$TAVE[datW$siteN == 4],
+     freq = FALSE, 
+     main = paste(levels(datW$NAME)[4]),
+     xlab = "Average daily temperature (degrees C)",
+     ylab = "Relative frequency", 
+     col = "mediumpurple",
+     border = "white")
+#add mean line with red (tomato3) color
+#and thickness of 3
+abline(v = mean(datW$TAVE[datW$siteN == 4], na.rm = TRUE),
+       col = "gray26",
+       lwd = 3)
+#add standard deviation line below the mean with red (tomato3) color
+#and thickness of 3
+abline(v = mean(datW$TAVE[datW$siteN == 4], na.rm=TRUE) - sd(datW$TAVE[datW$siteN == 2], na.rm=TRUE),
+       col = "gray26",
+       lty = 3,
+       lwd = 3)
+#add standard deviation line above the mean with red (tomato3) color
+#and thickness of 3
+abline(v = mean(datW$TAVE[datW$siteN == 4],na.rm=TRUE) + sd(datW$TAVE[datW$siteN == 2], na.rm=TRUE),
+       col = "gray26",
+       lty = 3,
+       lwd = 3)
+
+
+par(mfrow=c(1,1))
+###################################################
+##    Probability Distribution over Histogram    ##
+###################################################
+
+###################################################
+##              ABERDEEN                         ##
+###################################################
+#make a histogram for the first site in our levels
+#main= is the title name argument.
+#Here you want to paste the actual name of the factor not the numeric index
+#since that will be more meaningful. 
+#note I've named the histogram so I can reference it later
+h1 <- hist(datW$TAVE[datW$siteN == 1],
+           freq=FALSE, 
+           main = paste(levels(datW$NAME)[1]),
+           xlab = "Average daily temperature (degrees C)", 
+           ylab="Relative frequency",
+           col="grey50",
+           border="white")
+#the seq function generates a sequence of numbers that we can use to plot the normal across the range of temperature values
+x.plot <- seq(-10,30, length.out = 100)
+#the dnorm function will produce the probability density based on a mean and standard deviation.
+
+y.plot <-  dnorm(seq(-10,30, length.out = 100),
+                 mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE),
+                 sd(datW$TAVE[datW$siteN == 1],na.rm=TRUE))
+#create a density that is scaled to fit in the plot  since the density has a different range from the data density.
+#!!! this is helpful for putting multiple things on the same plot
+#!!! It might seem confusing at first. It means the maximum value of the plot is always the same between the two datasets on the plot. Here both plots share zero as a minimum.
+y.scaled <- (max(h1$density)/max(y.plot)) * y.plot
+
+#points function adds points or lines to a graph  
+#the first two arguements are the x coordinates and the y coordinates.
+
+points(x.plot,
+       y.scaled, 
+       type = "l", 
+       col = "royalblue3",
+       lwd = 4, 
+       lty = 2)
+
+
+
+# Documentation for normal distribution probability calculations
+help(dnorm)
+
+# Example probability calculation
+#pnorm(value to evaluate at (note this will evaluate for all values and below),mean, standard deviation)
+pnorm(0,
+      mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE),
+      sd(datW$TAVE[datW$siteN == 1],na.rm=TRUE))
+
+#pnorm with 5 gives me all probability (area of the curve) below 5 
+pnorm(5,
+      mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE),
+      sd(datW$TAVE[datW$siteN == 1],na.rm=TRUE))
+
+#pnorm with 5 gives me all probability (area of the curve) below 5 
+pnorm(5,
+      mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE),
+      sd(datW$TAVE[datW$siteN == 1],na.rm=TRUE))- pnorm(0,
+      mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE),
+      sd(datW$TAVE[datW$siteN == 1],na.rm=TRUE))
+
+#pnorm of 20 gives me all probability (area of the curve) below 20 
+#subtracting from one leaves me with the area above 20
+1 - pnorm(20,
+          mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE),
+          sd(datW$TAVE[datW$siteN == 1],na.rm=TRUE))
+
+#pnorm of 20 gives me all probability (area of the curve) below 20 
+#subtracting from one leaves me with the area above 20
+extreme.high <- qnorm(0.95,
+      mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE),
+      sd(datW$TAVE[datW$siteN == 1],na.rm=TRUE))
+
+#######################################
+##           Question 6              ##
+#######################################
+1 - pnorm(extreme.high,
+          mean(datW$TAVE[datW$siteN == 1]+4,na.rm=TRUE),
+          sd(datW$TAVE[datW$siteN == 1],na.rm=TRUE))
+
+
+#######################################
+##           Question 7              ##
+#######################################
+#make a histogram for the first site in our levels, Aberdeen
+#main = is the title name argument.
+#Here you want to paste the actual name of the factor not the numeric index
+#since that will be more meaningful.
+hist(datW$PRCP[datW$siteN == 1],
+     freq = FALSE, 
+     main = paste(levels(datW$NAME)[1]),
+     xlab = "Average daily precipitation",
+     ylab = "Relative frequency", 
+     col = "steelblue",
+     border = "white")
+#add mean line 
+#and thickness of 3
+abline(v = mean(datW$PRCP[datW$siteN == 1], na.rm = TRUE),
+       col = "gray26",
+       lwd = 3)
+#add standard deviation line below the mean
+#and thickness of 3
+abline(v = mean(datW$PRCP[datW$siteN == 1], na.rm=TRUE) - sd(datW$PRCP[datW$siteN == 1], na.rm=TRUE),
+       col = "gray26",
+       lty = 3,
+       lwd = 3)
+#add standard deviation line above the mean
+#and thickness of 3
+abline(v = mean(datW$PRCP[datW$siteN == 1],na.rm=TRUE) + sd(datW$PRCP[datW$siteN == 1], na.rm=TRUE),
+       col = "gray26",
+       lty = 3,
+       lwd = 3)
+
+#######################################
+##           Question 8              ##
+#######################################
+dat1 <- subset(datW,datW$siteN==1)
+avgPRCP1 <- aggregate(dat1$PRCP, by=list(dat1$year), FUN="sum",na.rm=TRUE)
+avgPRCP1
+
+dat2 <- subset(datW, datW$siteN == 2)
+avgPRCP2 <- aggregate(dat2$PRCP, by=list(dat2$year), FUN="sum", na.rm=TRUE)
+
+dat3 <- subset(datW, datW$siteN == 3)
+avgPRCP3 <- aggregate(dat3$PRCP, by=list(dat3$year), FUN="sum", na.rm=TRUE)
+
+dat4 <- subset(datW, datW$siteN == 4)
+avgPRCP4 <- aggregate(dat4$PRCP, by=list(dat4$year), FUN="sum", na.rm=TRUE)
+
+dat5 <- subset(datW, datW$siteN == 5)
+avgPRCP5 <- aggregate(dat5$PRCP, by=list(dat5$year), FUN="sum", na.rm=TRUE)
+
+#### HISTOGRAM ####
+hist(avgPRCP2$x,
+     freq = FALSE, 
+     main = paste(levels(datW$NAME)[2]),
+     xlab = "Annual Precipitation",
+     ylab = "Relative frequency", 
+     col = "steelblue",
+     border = "white")
+#add mean line 
+#and thickness of 3
+abline(v = mean(avgPRCP2$x, na.rm = TRUE),
+       col = "gray26",
+       lwd = 3)
+#add standard deviation line below the mean
+#and thickness of 3
+abline(v = mean(avgPRCP2$x, na.rm=TRUE) - sd(avgPRCP2$x, na.rm=TRUE),
+       col = "gray26",
+       lty = 3,
+       lwd = 3)
+#add standard deviation line above the mean
+#and thickness of 3
+abline(v = mean(avgPRCP2$x,na.rm=TRUE) + sd(avgPRCP2$x, na.rm=TRUE),
+       col = "gray26",
+       lty = 3,
+       lwd = 3)
+  
+#######################################
+##           Question 9              ##
+####################################### 
+mean1 <- mean(avgPRCP1$x, na.rm=TRUE)
+mean2 <- mean(avgPRCP2$x,na.rm=TRUE)
+mean3 <- mean(avgPRCP3$x,na.rm=TRUE)
+mean4 <- mean(avgPRCP4$x,na.rm=TRUE)
+mean5 <- mean(avgPRCP5$x,na.rm=TRUE)

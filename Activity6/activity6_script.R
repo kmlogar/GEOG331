ffbcf308f729731284974b68b797202987572b87
Kayla Logar <kaylal26@Kaylas-MacBook-Pro-2.local>
2020-03-31 11:10:26 -0400

    Activity 6

--- Activity6/activity6_script.R
+++ Activity6/activity6_script.R
@@ -9,10 +9,16 @@ library(rgeos)
 library(plyr)
 
 #read in files
-g1966 <- readOGR("Y:\\Students\\klogar\\a06\\GNPglaciers\\GNPglaciers_1966.shp")
-g1998 <- readOGR("Y:\\Students\\klogar\\a06\\GNPglaciers\\GNPglaciers_1998.shp")
-g2005 <- readOGR("Y:\\Students\\klogar\\a06\\GNPglaciers\\GNPglaciers_2005.shp")
-g2015 <- readOGR("Y:\\Students\\klogar\\a06\\GNPglaciers\\GNPglaciers_2015.shp")
+f1966=c("Y:\\Students\\klogar\\a06\\GNPglaciers\\GNPglaciers_1966.shp")
+f1998=c("Y:\\Students\\klogar\\a06\\GNPglaciers\\GNPglaciers_1998.shp")
+f2005=c("Y:\\Students\\klogar\\a06\\GNPglaciers\\GNPglaciers_2005.shp")
+f2015=c("Y:\\Students\\klogar\\a06\\GNPglaciers\\GNPglaciers_2015.shp")
+
+
+g1966 <- readOGR("/Users/kaylal26/Documents/ColgateAcademics/Spring2020/EnvironmentalDataScience/GitHub/GEOG331/Activity6/GNPglaciers/GNPglaciers_1966.shp")
+g1998 <- readOGR("/Users/kaylal26/Documents/ColgateAcademics/Spring2020/EnvironmentalDataScience/GitHub/GEOG331/Activity6/GNPglaciers/GNPglaciers_1998.shp")
+g2005 <- readOGR("/Users/kaylal26/Documents/ColgateAcademics/Spring2020/EnvironmentalDataScience/GitHub/GEOG331/Activity6/GNPglaciers/GNPglaciers_2005.shp")
+g2015 <- readOGR("/Users/kaylal26/Documents/ColgateAcademics/Spring2020/EnvironmentalDataScience/GitHub/GEOG331/Activity6/GNPglaciers/GNPglaciers_2015.shp")
 
 #investigate data about plots
 str(g2015)
@@ -40,9 +46,12 @@ g2015@data$GLACNAME <- ifelse(g2015@data$GLACNAME == "North Swiftcurrent Glacier
                                         as.character(g2015@data$GLACNAME)))
 
 #read in rgb imagery from landsat
-redL <- raster("Y:\\Students\\hkropp\\a06\\glacier_09_05_14\\l08_red.tif")
-greenL <- raster("Y:\\Students\\hkropp\\a06\\glacier_09_05_14\\l08_green.tif")
-blueL <- raster("Y:\\Students\\hkropp\\a06\\glacier_09_05_14\\l08_blue.tif")
+red.files <- c("Y:\\Students\\hkropp\\a06\\glacier_09_05_14\\l08_red.tif")
+green.files <- c("Y:\\Students\\hkropp\\a06\\glacier_09_05_14\\l08_green.tif")
+blue.files <- c("Y:\\Students\\hkropp\\a06\\glacier_09_05_14\\l08_blue.tif")
+redL <- raster("/Users/kaylal26/Documents/ColgateAcademics/Spring2020/EnvironmentalDataScience/GitHub/GEOG331/Activity6/glacier_09_05_14/l08_red.tif")
+greenL <- raster("/Users/kaylal26/Documents/ColgateAcademics/Spring2020/EnvironmentalDataScience/GitHub/GEOG331/Activity6/glacier_09_05_14/l08_green.tif")
+blueL <- raster("/Users/kaylal26/Documents/ColgateAcademics/Spring2020/EnvironmentalDataScience/GitHub/GEOG331/Activity6/glacier_09_05_14/l08_blue.tif")
 
 
 #check coordinate system
@@ -74,7 +83,7 @@ ndviYear <- seq(2003,2016)
 #read all files into a list
 NDVIraster <- list() 
 for(i in 1:length(ndviYear)){
-  NDVIraster[[i]] <- raster(paste0("Y:\\Students\\klogar\\a06\\NDVI\\NDVI_",ndviYear[i],".tif"))
+  NDVIraster[[i]] <- raster(paste0("/Users/kaylal26/Documents/ColgateAcademics/Spring2020/EnvironmentalDataScience/GitHub/GEOG331/Activity6/NDVI/NDVI_",ndviYear[i],".tif"))
   
 }
 
@@ -97,9 +106,10 @@ plot(NDVIraster[[1]])
 #zoomed out and only see two blips
 
 #plot side by side
+par(mai=c(1,1,1,1))
 par(mfrow=c(1,2))
 plot(NDVIraster[[1]],axes=TRUE)
-plot(g1966, axes=TRUE) 
+plot(g1966, axes=TRUE, add=FALSE) 
 
 ########################################
 
@@ -110,17 +120,19 @@ g1966p <- spTransform(g1966,NDVIraster[[1]]@crs)
 g1998p <- spTransform(g1998,NDVIraster[[1]]@crs)
 g2005p <- spTransform(g2005,NDVIraster[[1]]@crs)
 g2015p <- spTransform(g2015,NDVIraster[[1]]@crs)
+par(mai=c(1,1,1,1))
 plot(NDVIraster[[1]])
 
 #########################################
 ##            Question 4               ##
 #########################################
 
-plot(NDVIraster[[13]])
+par(mai=c(1,1,1,1))
+plot(NDVIraster[[13]], axes = FALSE, box=FALSE)
 plot(g2015p, col=NA, border="black",add = TRUE)
 
 
-##########################################
+#############################################
 
 #calculate area for all polygons
 #add directly into data table for each shapefile
@@ -150,4 +162,159 @@ for(i in 2:39){
 }   
 
 
+#########################################
+##            Question 5               ##
+#########################################
 
+#create column % change in area
+g2015p@data$percent.change <- ((g2015p@data$a2015m.sq - g1966p@data$a1966m.sq)/g1966p@data$a1966m.sq)*100
+
+#make spplot
+spplot(g2015p, "percent.change")
+
+#########################################
+
+
+diffPoly <- gDifference(g1966p, g2015p)
+plot(diffPoly)
+
+#plot with NDVI
+plot(NDVIraster[[13]], axes=FALSE, box=FALSE)
+plot(diffPoly,col="black", border=NA,add=TRUE)
+
+#########################################
+##            Question 6               ##
+#########################################
+
+#identify highest percentage loss
+most.loss <- min(g2015p@data$percent.change)
+
+#identify corresponding glacier
+max.loss.2015 <- subset(g2015p, g2015p$percent.change==most.loss)
+max.loss.name <- max.loss.2015$GLACNAME
+
+
+#subset remaining years to just boulder glacier
+max.loss.1966 <- subset(g1966p, g1966p$GLACNAME == max.loss.name)
+max.loss.1998 <- subset(g1998p, g1998p$GLACNAME == max.loss.name)
+max.loss.2005 <- subset(g2005p, g2005p$GLACNAME == max.loss.name)
+
+# note: glacial extent in 1966:
+#[-80123:-78850, 106841:107222]
+
+plot(NDVIraster[[13]], axes = FALSE, box=FALSE, xlim = c(-80250,-78700), ylim=c(106700,107300))
+plot(max.loss.2015, col=NA, border="black",add = TRUE)
+plot(max.loss.2005, col=NA, border="slateblue3",add = TRUE)
+plot(max.loss.1998, col=NA, border="steelblue4",add = TRUE)
+plot(max.loss.1966, col=NA, border="steelblue1",add = TRUE)
+title("Boulder Glacier (84.72% Loss)")
+
+
+#########################################
+
+
+#extract NDVI values
+NDVIdiff <- list()
+meanDiff <- numeric(0)
+#loop through all NDVI years
+for(i in 1:length(ndviYear)){
+  #get raster values in the difference polygon
+  NDVIdiff[[i]] <- extract(NDVIraster[[i]],diffPoly)[[1]]
+  #calculate the mean of the NDVI values
+  meanDiff[i] <- mean(NDVIdiff[[i]], na.rm=TRUE)
+}
+
+plot(ndviYear, meanDiff, type="b",
+     xlab= "Year",
+     ylab="Average NDVI (unitless)",
+     pch=19)
+
+#designate that NDVIraster list is a stack
+NDVIstack <- stack(NDVIraster)
+#set up lm function to apply to every cell
+#where x is the value of a cell
+#need to first skip NA values (like lakes)
+#if NA is missing in first raster, it is missing in all
+#so we can tell R to assign an NA rather than fitting the function
+timeT <- ndviYear
+fun <- function(x) {
+  if(is.na(x[1])){
+    NA}else{
+      #fit a regression and extract a slope
+      lm(x ~ timeT)$coefficients[2] }}
+#apply the slope function to the rasters
+NDVIfit <- calc(NDVIstack,fun)
+#plot the change in NDVI
+plot(NDVIfit, axes=FALSE)
+
+
+#########################################
+##            Question 7               ##
+#########################################
+
+# lower around white areas (water)
+# green areas are far from where glaciers are
+
+#########################################
+
+
+#buffer glaciers
+glacier500m <- gBuffer(g1966p,#data to buffer
+                       byid=TRUE,#keeps original shape id 
+                       width=500)#width in coordinate system units
+
+#convert to a raster
+buffRaster <- rasterize(glacier500m,#vector to convert to raster
+                        NDVIraster[[1]], #raster to match cells and extent
+                        field=glacier500m@data$GLACNAME, #field to convert to raster data
+                        background=0)#background value for missing data
+plot(buffRaster)
+
+#rasterize gralciers
+glacRaster <- rasterize(g1966p, NDVIraster[[1]], field=g1966p@data$GLACNAME, background=0)
+#subtract buffer from original glacier
+glacZones <- buffRaster - glacRaster
+plot(glacZones)
+
+#########################################
+##            Question 8               ##
+#########################################
+
+#open ended response...
+#gdifference - use on datasets
+#raster math - directly using on rasterized data
+
+#########################################
+
+
+meanChange <- zonal(NDVIfit, #NDVI function to summarize
+                    glacZones,#raster with zones
+                    "mean")#function to apply
+head(meanChange)
+
+
+#########################################
+##            Question 9               ##
+#########################################
+
+#remove zone w/o glaciers
+meanChange.noz <- meanChange[-1,]
+
+#add mean change col to 2015 data
+g2015p$meanChange <- meanChange.noz[,2]
+
+#plot data color coded by mean change
+spplot(g2015p,"meanChange")
+
+#most negative; very low --> decrease in veg over time
+
+#########################################
+
+
+#########################################
+##            Question 10              ##
+#########################################
+
+#somewhat inconclusive - analyze map before question 7 ; maybe tie in some info from line 
+# graph too --> generally, no trend; BUT some areas have significant inc/dec...
+# tie in some sort of discussion related to glaciers

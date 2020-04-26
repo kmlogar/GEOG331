964ecdde6dcfd775b5e748afb88a4bbc88127a9c
kmlogar <60156663+kmlogar@users.noreply.github.com>
2020-04-26 16:42:44 -0400

    Change Directories

--- Activity7/activity7_script.R
+++ Activity7/activity7_script.R
@@ -13,50 +13,50 @@ library(nnet)
 library(gdtools)
 
 #set up directory for oneida data folder
-#dirR <- "c:\\Users\\hkropp\\Google Drive\\courses\\Spring2020\\Environmental Data Science\\data\\oneida"
-dirK <- "/Users/kaylal26/Documents/ColgateAcademics/Spring2020/EnvironmentalDataScience/GitHub/GEOG331/Activity7/oneida"
+dirR <- "c:\\Users\\hkropp\\Google Drive\\courses\\Spring2020\\Environmental Data Science\\data\\oneida"
+#dirK <- "/Users/kaylal26/Documents/ColgateAcademics/Spring2020/EnvironmentalDataScience/GitHub/GEOG331/Activity7/oneida"
 
 #read in Sentinel data
 
-rdatB2 <- raster(paste0(dirK,"/sentinel/T18TVN_20190814T154911_B02_20m.tif"))
-rdatB3 <- raster(paste0(dirK,"/sentinel/T18TVN_20190814T154911_B03_20m.tif"))
-rdatB4 <- raster(paste0(dirK,"/sentinel/T18TVN_20190814T154911_B04_20m.tif"))
-rdatB5 <- raster(paste0(dirK,"/sentinel/T18TVN_20190814T154911_B05_20m.tif"))
-rdatB6 <- raster(paste0(dirK,"/sentinel/T18TVN_20190814T154911_B06_20m.tif"))
-rdatB7 <- raster(paste0(dirK,"/sentinel/T18TVN_20190814T154911_B07_20m.tif"))
-rdatB8 <- raster(paste0(dirK,"/sentinel/T18TVN_20190814T154911_B08_20m.tif"))
-rdatB11 <- raster(paste0(dirK,"/sentinel/T18TVN_20190814T154911_B11_20m.tif"))
-rdatB12 <- raster(paste0(dirK,"/sentinel/T18TVN_20190814T154911_B12_20m.tif"))
-clouds <- raster(paste0(dirK,"/sentinel/MSK_CLDPRB_20m.tif"))
+#rdatB2 <- raster(paste0(dirK,"/sentinel/T18TVN_20190814T154911_B02_20m.tif"))
+#rdatB3 <- raster(paste0(dirK,"/sentinel/T18TVN_20190814T154911_B03_20m.tif"))
+#rdatB4 <- raster(paste0(dirK,"/sentinel/T18TVN_20190814T154911_B04_20m.tif"))
+#rdatB5 <- raster(paste0(dirK,"/sentinel/T18TVN_20190814T154911_B05_20m.tif"))
+#rdatB6 <- raster(paste0(dirK,"/sentinel/T18TVN_20190814T154911_B06_20m.tif"))
+#rdatB7 <- raster(paste0(dirK,"/sentinel/T18TVN_20190814T154911_B07_20m.tif"))
+#rdatB8 <- raster(paste0(dirK,"/sentinel/T18TVN_20190814T154911_B08_20m.tif"))
+#rdatB11 <- raster(paste0(dirK,"/sentinel/T18TVN_20190814T154911_B11_20m.tif"))
+#rdatB12 <- raster(paste0(dirK,"/sentinel/T18TVN_20190814T154911_B12_20m.tif"))
+#clouds <- raster(paste0(dirK,"/sentinel/MSK_CLDPRB_20m.tif"))
 
 #read in validation data
 #here verbose=FALSE hiddes
-algae <- readOGR(paste0(dirK,"/Oneida/algae.shp"), verbose=FALSE)
-agri <- readOGR(paste0(dirK,"/Oneida/agriculture.shp"), verbose=FALSE)
-built <- readOGR(paste0(dirK,"/Oneida/built.shp"), verbose=FALSE)
-forest <- readOGR(paste0(dirK,"/Oneida/forest.shp"), verbose=FALSE)
-water <- readOGR(paste0(dirK,"/Oneida/water.shp"), verbose=FALSE)
-wetlands <- readOGR(paste0(dirK,"/Oneida/wetlands.shp"), verbose=FALSE)
+#algae <- readOGR(paste0(dirK,"/Oneida/algae.shp"), verbose=FALSE)
+#agri <- readOGR(paste0(dirK,"/Oneida/agriculture.shp"), verbose=FALSE)
+#built <- readOGR(paste0(dirK,"/Oneida/built.shp"), verbose=FALSE)
+#forest <- readOGR(paste0(dirK,"/Oneida/forest.shp"), verbose=FALSE)
+#water <- readOGR(paste0(dirK,"/Oneida/water.shp"), verbose=FALSE)
+#wetlands <- readOGR(paste0(dirK,"/Oneida/wetlands.shp"), verbose=FALSE)
 
-# rdatB2 <- raster(paste0(dirR,"\\sentinel\\T18TVN_20190814T154911_B02_20m.tif"))
-# rdatB3 <- raster(paste0(dirR,"\\sentinel\\T18TVN_20190814T154911_B03_20m.tif"))
-# rdatB4 <- raster(paste0(dirR,"\\sentinel\\T18TVN_20190814T154911_B04_20m.tif"))
-# rdatB5 <- raster(paste0(dirR,"\\sentinel\\T18TVN_20190814T154911_B05_20m.tif"))
-# rdatB6 <- raster(paste0(dirR,"\\sentinel\\T18TVN_20190814T154911_B06_20m.tif"))
-# rdatB7 <- raster(paste0(dirR,"\\sentinel\\T18TVN_20190814T154911_B07_20m.tif"))
-# rdatB8 <- raster(paste0(dirR,"\\sentinel\\T18TVN_20190814T154911_B08_20m.tif"))
-# rdatB11 <- raster(paste0(dirR,"\\sentinel\\T18TVN_20190814T154911_B11_20m.tif"))
-# rdatB12 <- raster(paste0(dirR,"\\sentinel\\T18TVN_20190814T154911_B12_20m.tif"))
-# clouds <- raster(paste0(dirR,"\\sentinel\\MSK_CLDPRB_20m.tif"))
-# 
-# #read in validation data
-# #here verbose=FALSE hiddes
-# algae <- readOGR(paste0(dirR,"\\Oneida\\algae.shp"), verbose=FALSE)
-# agri <- readOGR(paste0(dirR,"\\Oneida\\agriculture.shp"), verbose=FALSE)
-# built <- readOGR(paste0(dirR,"\\Oneida\\built.shp"), verbose=FALSE)
-# forest <- readOGR(paste0(dirR,"\\Oneida\\forest.shp"), verbose=FALSE)
-# water <- readOGR(paste0(dirR,"\\Oneida\\water.shp"), verbose=FALSE)
-# wetlands <- readOGR(paste0(dirR,"\\Oneida\\wetlands.shp"), verbose=FALSE)
+rdatB2 <- raster(paste0(dirR,"\\sentinel\\T18TVN_20190814T154911_B02_20m.tif"))
+rdatB3 <- raster(paste0(dirR,"\\sentinel\\T18TVN_20190814T154911_B03_20m.tif"))
+rdatB4 <- raster(paste0(dirR,"\\sentinel\\T18TVN_20190814T154911_B04_20m.tif"))
+rdatB5 <- raster(paste0(dirR,"\\sentinel\\T18TVN_20190814T154911_B05_20m.tif"))
+rdatB6 <- raster(paste0(dirR,"\\sentinel\\T18TVN_20190814T154911_B06_20m.tif"))
+rdatB7 <- raster(paste0(dirR,"\\sentinel\\T18TVN_20190814T154911_B07_20m.tif"))
+rdatB8 <- raster(paste0(dirR,"\\sentinel\\T18TVN_20190814T154911_B08_20m.tif"))
+rdatB11 <- raster(paste0(dirR,"\\sentinel\\T18TVN_20190814T154911_B11_20m.tif"))
+rdatB12 <- raster(paste0(dirR,"\\sentinel\\T18TVN_20190814T154911_B12_20m.tif"))
+clouds <- raster(paste0(dirR,"\\sentinel\\MSK_CLDPRB_20m.tif"))
+ 
+#read in validation data
+#here verbose=FALSE hiddes
+algae <- readOGR(paste0(dirR,"\\Oneida\\algae.shp"), verbose=FALSE)
+agri <- readOGR(paste0(dirR,"\\Oneida\\agriculture.shp"), verbose=FALSE)
+built <- readOGR(paste0(dirR,"\\Oneida\\built.shp"), verbose=FALSE)
+forest <- readOGR(paste0(dirR,"\\Oneida\\forest.shp"), verbose=FALSE)
+water <- readOGR(paste0(dirR,"\\Oneida\\water.shp"), verbose=FALSE)
+wetlands <- readOGR(paste0(dirR,"\\Oneida\\wetlands.shp"), verbose=FALSE)
 
 #stack red green blue
 rgbS <- stack(rdatB4,rdatB3,rdatB2)
